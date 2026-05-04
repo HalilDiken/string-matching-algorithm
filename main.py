690f3d27cefbbdf5dcebe9a5c918e408601eeb44
Halil Diken <batman147batman@gmail.com>
2026-05-04 20:24:58 +0300

    maini güncelledim ve htmlleri ekledim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
+import os
+import urllib.request
 from utils import createMarkedHTML
-from algorithms import brute_force
+from algorithms import brute_force,horspool,boyer_moore
 
 # it is our template for print all conclusion for all algorithm
 def test_algorithm (algo_name,algo_func,text,pattern,output_filename):
@@ -29,7 +31,88 @@ def test_algorithm (algo_name,algo_func,text,pattern,output_filename):
 
 
 if __name__ == "__main__":
-        test_text = "<HTML> <BODY> WHICH FINALLY HALTS. AT THAT POINT</BODY> </HTML>"
-        pattern = "AT THAT"
+    
+    # ---------------------------------------------------------
+    # PART 1 : 4 Small Test Case for All algorithms
+    # ---------------------------------------------------------
+    test_cases = [
+        ("TC1", "<HTML><BODY>WHICH_FINALLY_HALTS. _ _ AT_THAT POINT </BODY></HTML>", "  "),
+        ("TC2", "<HTML><BODY>ABC_AT_THAT_XYZ_AT_THAT_END</BODY></HTML>", "AT_THAT"),
+        ("TC3", "<HTML><BODY>AAAAAA</BODY></HTML>", "AAA"),
+        ("TC4", "<HTML><BODY>THIS_IS_A_TEST</BODY></HTML>", "XYZ")
+    ]
 
-        test_algorithm("Brute-Force",brute_force,test_text,pattern,"test1_output.html")
+    algorithms = [
+        ("Brute-Force", brute_force, "BF"),
+        ("Horspool", horspool, "HP"),
+        ("Boyer-Moore", boyer_moore, "BM")
+    ]
+
+    print("="*50 + "\n Small test case begin\n" + "="*50)
+
+    for tc_name, text, pattern in test_cases:
+        print(f"\n[{tc_name}] Test is running - Text: {text}")
+        for algo_name, algo_func, short_name in algorithms:
+            output_name = f"{tc_name}_{short_name}.html"
+            test_algorithm(algo_name, algo_func, text, pattern, output_name)
+
+    # ---------------------------------------------------------
+    # PART 2: HTML -> Type 1&2 -> 1) Karamazov Brothers 
+    #                             2) Crime and Punishment
+    #                             3) Turkey
+    #                             4) Bit String 1
+    #                             5) Bit String 2
+    #                             6) Bit String 3
+    # ---------------------------------------------------------
+    print("\n" + "="*50 + "\n TYPE 1: Meaningful HTML \n" + "="*50)
+    
+    
+    html_sources = [
+        {
+            "id": "CrimeAndPunishment",
+            "file_name": "Crime and Punishment.html",
+            "patterns": ["Raskolnikov", "the", "murder"]
+        },
+        {
+            "id": "Karamazov",
+            "file_name": "The Brothers Karamazov.html", 
+            "patterns": ["Karamazov", "the", "characteristics"]
+        },
+        {
+            "id": "Turkey",
+            "file_name": "Turkey.html",
+            "patterns": ["history", "culture", "the"]
+        },
+        {
+            "id": "BitString1",
+            "file_name": "bit_string1.html",
+            "patterns": ["10101", "00000"] 
+        },
+        {
+            "id": "BitString2",
+            "file_name": "bit_string2.html",
+            "patterns": ["11111", "10001"] 
+        },
+        {
+            "id": "BitString3",
+            "file_name": "bit_string3.html",
+            "patterns": ["01010", "11011"] 
+        }
+    ]
+
+    for item in html_sources:
+        print(f"\n" + "-"*40)
+        print(f" PROCESSING: {item['id']} ")
+        print("-" * 40)
+        
+ 
+        # Read file and send to algorithms
+        with open(item["file_name"], "r", encoding="utf-8") as file:
+            html_text = file.read()
+
+        # Look all algorithms for each pattern
+        for pattern in item["patterns"]:
+            print(f"\n>>> '{pattern}' SEARCHING IN {item['id']} <<<")
+            for algo_name, algo_func, short_name in algorithms:
+                output_name = f"{item['id']}_{pattern}_{short_name}.html"
+                test_algorithm(algo_name, algo_func, html_text, pattern, output_name)
